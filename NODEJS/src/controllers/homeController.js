62ab5ab8d6346e25fa4c2883d4b94510ccaa3425
Trinh Tram Anh <tramanh.912.pch@gmail.com>
2023-03-20 18:06:13 +0700

    vi#35

--- NODEJS/src/controllers/homeController.js
+++ NODEJS/src/controllers/homeController.js
@@ -31,16 +31,53 @@ let postCRUD = async(req,res) =>{
     return res.send('post crud from server');
 }
 
-let displayCRUD = async (req,res) => {
+let displayGetCRUD = async (req,res) => {
     let data = await CRUDService.getAllUser();
-    console.log('--------------------------------');
-    console.log(data);
-    console.log('--------------------------------');
+    // console.log('--------------------------------');
+    // console.log(data);
+    // console.log('--------------------------------');
     return res.render('displayCRUD.ejs', {
         dataTable:data
     });
 }
 
+let getEditCRUD = async (req,res) =>{
+    let userId = req.query.id;
+    if(userId){
+        let userData = await CRUDService.getUserInfoById(userId);
+        //check user data not found
+        //let userData
+        return res.render('editCRUD.ejs', {
+            user: userData
+        });
+    }
+    else{
+        return res.send('User not found!!')
+
+    }
+
+}
+
+let putCRUD = async (req,res) =>{
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+    return res.render('displayCRUD.ejs', {
+        dataTable:allUsers
+    });
+
+}
+
+let deleteCRUD = async (req, res) => {
+    let id =req.query.id;
+    if(id){
+        await CRUDService.deleteUserById(id);
+        return res.send('Delete user success')
+    }
+    else{
+        return res.send('User not found')
+    }
+
+}
 // object:{
 //     key:''
 //     value:''
@@ -51,5 +88,8 @@ module.exports={
     getAboutPage: getAboutPage,
     getCRUD: getCRUD,
     postCRUD: postCRUD,
-    displayCRUD: displayCRUD,
+    displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
+    deleteCRUD: deleteCRUD,
 }
--- NODEJS/src/route/web.js
+++ NODEJS/src/route/web.js
@@ -1,20 +1,24 @@
 import express from "express";
 import homeController from "../controllers/homeController";
+import userController from "../controllers/userController";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
+  router.get("/", homeController.getHomePage);
+  //rest api
+  router.get("/about", homeController.getAboutPage);
+  router.get("/crud", homeController.getCRUD);
 
-    router.get('/', homeController.getHomePage);
-    //rest api
-    router.get('/about' , homeController.getAboutPage);
-    router.get('/crud' , homeController.getCRUD);
+  router.post("/post-crud", homeController.postCRUD);
+  router.get("/get-crud", homeController.displayGetCRUD);
 
-    router.post('/post-crud' , homeController.postCRUD);
-    router.get('/get-crud', homeController.displayCRUD);
+  router.get("/edit-crud", homeController.getEditCRUD);
+  router.post("/put-crud", homeController.putCRUD);
+  router.get("/delete-crud", homeController.deleteCRUD);
 
-
-    return app.use("/",router);
-}
+  router.post("/api/login",userController.handleLogin);
+  return app.use("/", router);
+};
 
 module.exports = initWebRoutes;
