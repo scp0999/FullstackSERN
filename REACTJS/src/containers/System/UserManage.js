fe7a3a4c01030e7834c32ace2ce754a1e2cedb68
Trinh Tram Anh <tramanh.912.pch@gmail.com>
2023-03-22 09:33:13 +0700

    vi#38

--- REACTJS/src/containers/System/UserManage.js
+++ REACTJS/src/containers/System/UserManage.js
@@ -1,33 +1,77 @@
-import React, { Component } from 'react';
-import { FormattedMessage } from 'react-intl';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { FormattedMessage } from "react-intl";
+import { connect } from "react-redux";
+import "./UserManage.scss";
+import { getAllUsers } from "../../services/userService";
 class UserManage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      arrUsers: [],
+    };
+  }
 
-    state = {
-
+  async componentDidMount() {
+    let response = await getAllUsers("ALL");
+    if (response && response.errCode === 0) {
+      this.setState({
+        arrUsers: response.users,
+      });
     }
+  }
 
-    componentDidMount() {
+  /**Life cycle
+   * Run component
+   * 1. Run construct -> init state
+   * 2. Did mount (set state)
+   * 3.Render
+   *
+   */
+  render() {
+    console.log("check render", this.state);
+    let arrUsers = this.state.arrUsers;
+    return (
+      <div className="user-container">
+        <div className="title text-center">Manage users with SCP999</div>
+        <div className="users-table mt-3 mx-1">
+          <table id="customers">
+            <tr>
+              <th>Email</th>
+              <th>First name</th>
+              <th>Last name</th>
+              <th>Address</th>
+              <th>Action</th>
+            </tr>
+            { arrUsers && arrUsers.map((item,index) =>{
+                console.log('SCP999 check map' , item,index)
+                return(
+                    <tr key = {index}>
+                    <td>{item.email}</td>
+                    <td>{item.firstName}</td>
+                    <td>{item.lastName}</td>
+                    <td>{item.address}</td>
+                    <td>
+                        <button className='btn-edit'><i className='fas fa fa-pencil-alt'></i></button>
+                        <button className='btn-delete'><i className='fas fa fa-trash'></i></button>
+                    </td>
+                  </tr>
+                )
+            })
+            }
 
-    }
-
-
-    render() {
-        return (
-            <div className="text-center">Manage users</div>
-        );
-    }
-
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-    };
+const mapStateToProps = (state) => {
+  return {};
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
