307ec27463eaf4010c0cfda08c828281771077bc
Trinh Tram Anh <tramanh.912.pch@gmail.com>
2023-05-15 16:40:58 +0700

    vi#74

--- REACTJS/src/services/userService.js
+++ REACTJS/src/services/userService.js
@@ -48,6 +48,10 @@ const getDetailInforDoctor = (inputId) => {
   return axios.get(`/api/get-detail-doctor-by-id?id=${inputId}`);
 };
 
+const saveBulkScheduleDoctor = (data) => {
+  return axios.post(`/api/bulk-create-schedule`,data);
+};
+
 export {
   handleLoginApi,
   getAllUsers,
@@ -59,4 +63,5 @@ export {
   getAllDoctors,
   saveDetailDoctorService,
   getDetailInforDoctor,
+  saveBulkScheduleDoctor,
 };
