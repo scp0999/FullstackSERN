5f5317bf59c4914d8d72837299aa821330f3065e
Trinh Tram Anh <tramanh.912.pch@gmail.com>
2023-05-17 19:07:26 +0700

    vi#83

--- NODEJS/src/controllers/doctorController.js
+++ NODEJS/src/controllers/doctorController.js
@@ -97,6 +97,21 @@ let getExtraInforDoctorById = async (req,res) => {
   }
 }
 
+let getProfileDoctorById = async (req, res) => {
+  try {
+    let infor = await doctorService.getProfileDoctorById(req.query.doctorId);
+    return res.status(200).json(
+      infor
+    )
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errCode: -1,
+      errMessage: "Error from server",
+    });
+  }
+}
+
 module.exports = {
   getTopDoctorHome: getTopDoctorHome,
   getAllDoctors: getAllDoctors,
@@ -105,4 +120,5 @@ module.exports = {
   bulkCreateSchedule: bulkCreateSchedule,
   getScheduleByDate: getScheduleByDate,
   getExtraInforDoctorById: getExtraInforDoctorById,
+  getProfileDoctorById: getProfileDoctorById,
 };
