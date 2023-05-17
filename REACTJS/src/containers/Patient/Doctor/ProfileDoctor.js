113d0adb77f2e11e491e3ed468d100b1f5e9bc87
Trinh Tram Anh <tramanh.912.pch@gmail.com>
2023-05-18 01:30:00 +0700

    vi#85

--- REACTJS/src/containers/Patient/Doctor/ProfileDoctor.js
+++ REACTJS/src/containers/Patient/Doctor/ProfileDoctor.js
@@ -58,7 +58,7 @@ class ProfileDoctor extends Component {
                 return (
                     <>
                         <div>{time} - {date} </div>
-                        <div>Miễn phí đặt lịch</div>
+                        <div><FormattedMessage id="patient.booking-modal.priceBooking"/></div>
 
                     </>
                 )
@@ -110,7 +110,7 @@ class ProfileDoctor extends Component {
                 </div>
 
                 <div className="price">
-                    Giá khám:
+                    <FormattedMessage id='patient.booking-modal.price'/>
                     {dataProfile && dataProfile.Doctor_Infor && language === LANGUAGES.VI && (
                           <NumberFormat
                             className="currency"
--- REACTJS/src/services/userService.js
+++ REACTJS/src/services/userService.js
@@ -63,6 +63,10 @@ const getExtraInforDoctorById = (doctorId) => {
 const getProfileDoctorById = (doctorId) =>{
   return axios.get(`/api/get-profile-doctor-by-id?doctorId=${doctorId}`);
 }
+
+const postPatientBookAppointment = (data) =>{
+  return axios.post(`/api/patient-book-appointment, data`);
+}
 export {
   handleLoginApi, getAllUsers,
   createNewUserService, deleteUserService,
@@ -70,5 +74,5 @@ export {
   getAllDoctors, saveDetailDoctorService, 
   getDetailInforDoctor, saveBulkScheduleDoctor,
   getScheduleDoctorByDate, getExtraInforDoctorById,
-  getProfileDoctorById
+  getProfileDoctorById, postPatientBookAppointment
 };
