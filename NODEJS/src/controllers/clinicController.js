6190080e14b787a479aaa943b8f41a9ef1a2eb2b
Trinh Tram Anh <tramanh.912.pch@gmail.com>
2023-05-19 16:56:23 +0700

    vi#97

--- NODEJS/src/controllers/clinicController.js
+++ NODEJS/src/controllers/clinicController.js
@@ -1,10 +1,10 @@
 /** @format */
 
-import clinicServices from "../services/clinicService";
+import clinicService from "../services/clinicService";
 
 let createClinic = async (req, res) => {
   try {
-    let infor = await clinicServices.createClinic(req.body);
+    let infor = await clinicService.createClinic(req.body);
     return res.status(200).json(infor);
   } catch (e) {
     console.log(e);
@@ -15,34 +15,34 @@ let createClinic = async (req, res) => {
   }
 };
 
-// let getAllClinic = async (req, res) => {
-//   try {
-//     let infor = await ClinicServices.getAllClinic();
-//     return res.status(200).json(infor);
-//   } catch (e) {
-//     console.log(e);
-//     return res.status(200).json({
-//       errCode: -1,
-//       errMessage: "Error from the server",
-//     });
-//   }
-// };
+let getAllClinic = async (req, res) => {
+  try {
+    let infor = await clinicService.getAllClinic();
+    return res.status(200).json(infor);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errCode: -1,
+      errMessage: "Error from the server",
+    });
+  }
+};
 
-// let getDetailClinicById = async (req, res) => {
-//   try {
-//     let infor = await ClinicServices.getDetailClinicById(req.query.id, req.query.location);
-//     return res.status(200).json(infor);
-//   } catch (e) {
-//     console.log(e);
-//     return res.status(200).json({
-//       errCode: -1,
-//       errMessage: "Error from the server",
-//     });
-//   }
-// };
+let getDetailClinicById = async (req, res) => {
+  try {
+    let infor = await clinicService.getDetailClinicById(req.query.id);
+    return res.status(200).json(infor);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errCode: -1,
+      errMessage: "Error from the server",
+    });
+  }
+};
 
 module.exports = {
   createClinic: createClinic,
-//   getAllClinic: getAllClinic,
-//   getDetailClinicById: getDetailClinicById,
+  getAllClinic: getAllClinic,
+  getDetailClinicById: getDetailClinicById,
 };
